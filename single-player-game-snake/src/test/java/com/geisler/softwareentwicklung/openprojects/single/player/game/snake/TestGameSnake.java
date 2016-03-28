d02a4a4bae7f7af86f7a6680e0ce8c00624eb5fc
Nico Geisler <hardandsoft@gmx.de>
2016-03-28 17:12:07 +0200

    - new single player game snake, should be easier to design and implement with tests and clean code

---
+++ single-player-game-snake/src/test/java/com/geisler/softwareentwicklung/openprojects/single/player/game/snake/TestGameSnake.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2015, Nico Geisler Softwareentwicklung
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: no conditions.
+ */
+package com.geisler.softwareentwicklung.openprojects.single.player.game.snake;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ngeis
+ */
+
+
+public class TestGameSnake {
+    
+    public TestGameSnake() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
---
+++ single-player-game-snake/src/test/java/com/geisler/softwareentwicklung/openprojects/single/player/game/snake/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2015, Nico Geisler Softwareentwicklung
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: no conditions.
+ */
+/**
+ * Package for tests of the single player game snake.
+ */
+package com.geisler.softwareentwicklung.openprojects.single.player.game.snake;
